473517bfc4e44f42518ca99ebae0377655c6952a
Anis Bellil <anissbellil13@gmail.com>
2022-05-07 23:18:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './assets/styles/styles.css'
+
+function App() {
+  return (
+    <div className="App">
+       <p>app</p>
+       <Routes>
+        <Route></Route>
+       </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import { Header } from "./Header"
+import { Play } from "./Play"
+import { Game } from "./Game"
+import { Modal } from "./Modal"
+import { Footer } from "./Footer"
+
+export {Header, Play, Game, Modal, Footer}
